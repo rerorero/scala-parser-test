d2715eee399e141550fa2b10ab5db21d6a4e1e4f
rerorero <nato9598@hotmail.co.jp>
2015-02-04 16:34:43 +0900

    add

--- Main.scala
+++ Main.scala
@@ -1,37 +1,100 @@
+
 import scala.util.parsing.combinator._
-import util.parsing.combinator.syntactical.StandardTokenParsers
+import scala.util.parsing.combinator.lexical._
+import scala.util.parsing.combinator.syntactical._
+import scala.util.parsing.combinator.token._
 
-//////////////////////////////////////////
-// Parser/Lexer Base
-//////////////////////////////////////////
-trait MyParserBase extends StandardTokenParsers 
-
+// utilities
+trait PrettyPrinters {
+  // TODO: escape
+  protected def _q(s: String): String = "\"" + s + "\""
+}
 
 //////////////////////////////////////////
 // AST
 //////////////////////////////////////////
 object AST {
-  sealed abstract class Expr
 
-  case class Equal(e1: Expr, e2: Expr) extends Expr
+  trait Node extends PrettyPrinters
+
+  case class Assign(name: String, e: Node) extends Node
+  case class Var(name: String) extends Node
+  case class PrintLn(e: Node) extends Node
+  case class Program(l: Seq[Node]) extends Node
+
+  case class Lit(v: Any) extends Node {
+    override def toString = "NODE :: " + v match {
+      case _:String => "\"" + v + "\""
+      case _ => v.toString
+    }
+  }
+
+  case class IfExpr(e1: Node, e2: Node, e3: Node) extends Node
+
+  case class Block(l : Seq[Node]) extends Node
+
+  case class IfClause(condition: Node, block: Node) extends Node
+  case class ElseClause(block: Node) extends Node
+  case class IfElse(ifElem: IfClause, elElem: Option[ElseClause]) extends Node
 
 }
 
 
+//////////////////////////////////////////
+// Parser Base
+//////////////////////////////////////////
+trait MyParserBase extends StandardTokenParsers
+class MyLexical extends StdLexical
 
 //////////////////////////////////////////
 // Parsing
 //////////////////////////////////////////
 object MyParser extends MyParserBase {
+
   import AST._
 
-  def parse(input: String) : Either[String, Equal] = phrase(program)(new lexical.Scanner(input)) match {
+  override val lexical = new MyLexical
+
+  // parse
+  def parse(input: String) : Either[String, Node] = phrase(program)(new lexical.Scanner(input)) match {
     case Success(ast, _) => Right(ast)
     case e: NoSuccess => Left("parser error:" + e.msg)
   }
 
-  lazy val program = "===" ^^^ Equal
+  val functions = ("println" split ' ')
+  val controlStatements = ("if true false" split ' ')
 
+  lexical.reserved ++= functions
+  lexical.reserved ++= controlStatements 
+  lexical.delimiters ++=  ("+ - * / ( ) { } , == = < > <> <= >=" split ' ')
+
+  // parser
+  type P[+T] = Parser[T]
+
+  lazy val program      = rep(statement) ^^ (Program(_))
+
+  lazy val statement    = expr | ifElse
+
+  lazy val expr:P[Node] = assign | println
+
+  lazy val println      = "println" ~> simpleExpr         ^^ (PrintLn(_))
+
+  lazy val assign       = ident ~ ("=" ~> simpleExpr)     ^^ { case i ~ e => Assign(i, e) }
+
+  lazy val simpleExpr   = ( ident               ^^ Var
+                          | numericLit          ^^ { x => Lit(x.toInt) }
+                          | stringLit           ^^ Lit
+                          | "true"              ^^ { _ => Lit(true) }
+                          | "false"             ^^ { _ => Lit(false) }
+                          | "(" ~> expr <~ ")"
+                          | failure ("Expression expected")
+                          )
+
+  lazy val block        = "{" ~> rep(expr) <~ "}"  ^^ (Block(_))
+
+  lazy val ifClause           = "if" ~> "(" ~ simpleExpr ~ ")" ~ (block | expr)  ^^ { case _ ~ ex ~ _ ~ bl => IfClause(ex, bl) }
+  lazy val elseClause         = "else" ~ (block | expr)                   ^^ { case _ ~ elem => ElseClause(elem) }
+  lazy val ifElse             = ifClause ~ opt(elseClause) ^^ { case e1 ~ e2 => IfElse(e1, e2) }
 }
 
 
@@ -39,6 +102,79 @@ object MyParser extends MyParserBase {
 //////////////////////////////////////////
 // Interpreter 
 //////////////////////////////////////////
-
 object MyInterpreter {
+  import AST._
+
+  type VarName = String
+  type VarValue = Any
+  type Environment = collection.immutable.Map[VarName, VarValue]
+
+  val initEnv: Environment = Map()
+
+  class Context(private var env: Environment = initEnv) {
+
+    override def toString = "Env: " + env.mkString(", ")
+
+    def eval(e: Node): Any = e match {
+      case Program(nodes) => nodes foreach eval
+
+      case Block(nodes) => nodes foreach eval
+
+      case Assign(id, node) => env += (id -> eval(node))
+
+      case Var(id) => env getOrElse(id, error("Undefined var " + id))
+
+      case PrintLn(v) => eval(v) match {
+        case value : String => println("println :: " + value)
+        case value => println("println :: [" + value.getClass.toString + "] " + value.toString)
+      }
+
+      case Lit(v) => v
+
+      case IfElse(ifElem: IfClause,  elElem: Option[ElseClause]) => {
+        val condVal = eval(ifElem.condition)
+        if (isInstanceOf[Boolean]) {
+          throw new Exception("Boolean condition expected." + condVal)
+        }else{
+          if (condVal.asInstanceOf[Boolean]) {
+            eval(ifElem.block)
+          }else{
+            elelem map eval(e.block)
+          }
+        }
+      }
+    }
+  }
+
+  def eval(ast: Node): Unit = {
+    (new Context) eval ast
+  }
 }
+
+
+//////////////////////////////////////////
+// Main
+//////////////////////////////////////////
+object Main extends App {
+
+  val sample1 = """
+    x = 99
+    println x
+
+    if (true) { 
+      x = 100
+      println "that is true!"
+    }
+
+    println x
+  """
+
+  val parser = MyParser
+  val interpreter = MyInterpreter
+
+  parser.parse(sample1) match {
+    case Right(ast) => interpreter.eval(ast)
+    case Left(msg) => println(msg)
+  }
+}
+
